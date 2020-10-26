5a788575de6e175603dae8087ce3153d991eb02a
JanotLeLapin <joseph300905@gmail.com>
2020-10-26 21:56:09 +0100

    Remove TypeScript output

--- dist/Structures/Channel.d.ts
+++
@@ -1,7 +0,0 @@
-import Bot from '../Bot/Bot';
-export default class Channel {
-    protected b: Bot;
-    protected token: string;
-    id: string;
-    constructor(data: any, bot: Bot, token: string);
-}
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Bot from './Bot/Bot';
-import User from './Structures/User';
-import Guild from './Structures/Guild';
-import Channel from './Structures/Channel';
-import Message from './Structures/Message';
-import Embed from './Structures/Embed';
-export { Bot, User, Guild, Channel, Message, Embed, };
