5cb070f6838f8c6df62bad925917806e474593d8
JanotLeLapin <joseph300905@gmail.com>
2020-10-26 18:45:16 +0100

    Create Bot class

---
+++ lib/Bot/Bot.ts
@@ -0,0 +1,167 @@
+import { client as WebSocketClient, connection } from 'websocket';
+import zlib from 'zlib';
+
+import User from '../Structures/User';
+import Guild from '../Structures/Guild';
+import Message from '../Structures/Message';
+
+interface Presence {
+    name: string;
+    type?: 'playing' | 'streaming' | 'listening' | 'watching';
+    status?: 'online' | 'dnd' | 'idle' | 'invisible' | 'offline';
+}
+
+interface EventListener {
+    ready: () => void;
+    error: (err: Error) => void;
+    message: (message: Message) => void;
+    guildCreate: (guild: Guild) => void;
+}
+interface EventEmitter {
+    ready: undefined;
+    error: Error;
+    message: Message;
+    guildCreate: Guild;
+}
+
+interface Error {
+    code?: number;
+    message: string;
+}
+
+let seq: number | null = null;
+const handler = (data: any, bot: Bot, token: string, connection: connection) => {
+    seq = data.s || null;
+    if (data.op == 10) {
+        setInterval(() => connection.send(JSON.stringify({
+            op: 1,
+            d: seq,
+        })), data.d.heartbeat_interval);
+    }
+    switch (data.t) {
+        case 'READY':
+            bot.user = new User(data.d.user, bot, token);
+            return bot.emit('ready');
+        case 'MESSAGE_CREATE':
+            new Message(data.d, bot, token);
+            break;
+        case 'GUILD_CREATE':
+            new Guild(data.d, bot, token);
+            break;
+    }
+}
+
+export default class Bot {
+    private connection?: connection;
+
+    user: User = new User({}, this, '');
+
+    guilds: Guild[] = [];
+
+    /**
+     * @description Connects the bot
+     * @param {string} token Your bot token
+     */
+    connect(token: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.connection) return reject('Already authenticated.');
+            const client = new WebSocketClient()
+            client.connect('wss://gateway.discord.gg/?v=6&encoding=json');
+            client.on('connect', connection => {
+                this.connection = connection;
+                connection.send(JSON.stringify({
+                    op: 2,
+                    d: {
+                        token: token,
+                        intents: 32509,
+                        properties: {
+                            $os: 'linux',
+                            $browser: 'disco',
+                            $device: 'disco',
+                        },
+                        compress: true,
+                        large_threshold: 250,
+                        guild_subscriptions: false,
+                        shard: [0, 1],
+                    },
+                }));
+                connection.on('message', data => {
+                    if (data.utf8Data) {
+                        const res: any = JSON.parse(data.utf8Data);
+                        handler(res, this, token, connection);
+                        if (res.t === 'READY') resolve();
+                    }
+                    if (data.binaryData) {
+                        zlib.unzip(data.binaryData, (err, buffer) => {
+                            if (err) return reject(err);
+                            const res: any = JSON.parse(buffer.toString('utf-8'));
+                            handler(res, this, token, connection);
+                            if (res.t === 'READY') resolve();
+                        });
+                    }
+                });
+                connection.on('close', (code, desc) => {
+                    this.emit('error', {
+                        code,
+                        message: desc, 
+                    });
+                });
+            });
+        })
+    }
+
+    /**
+     * @description Update the bot's activity
+     * @param {Presence} presence Presence object
+     * @example ```js
+     * bot.setPresence({ name: 'New Presence', type: 'watching', status: 'idle' })
+     * ```
+     */
+    setPresence(presence: Presence) {
+        this.connection?.send(JSON.stringify({
+            op: 3,
+            d: {
+                since: 0,
+                activities: [
+                    {
+                        name: presence.name,
+                        type: ['playing', 'streaming', 'listening', 'watching'].indexOf(presence.type || 'playing') || 0,
+                    },
+                ],
+                status: presence.status || 'online',
+                afk: false,
+            }
+        }));
+    }
+
+    handlers: any = {};
+    /**
+     * @description Listen for a specific event
+     * @param eventName The name of the event to listen to
+     * @param handler The function to run when this event is fired
+     * @example ```js
+     * bot.on('guildCreate', guild => console.log(guild.name));
+     * ```
+     */
+    on<K extends keyof EventListener>(eventName: K, handler: EventListener[K]): void {
+        if (!this.handlers[eventName]) this.handlers[eventName] = [];
+        this.handlers[eventName].push(handler);
+    }
+
+    /**
+     * @description Fire a specific event
+     * @param eventName The name of the event to fire
+     * @param data The data to bind to this event
+     * @example ```js
+     * bot.emit('error', { code: 123, message: 'My custom error' });
+     * ```
+     * You can now listen to this event
+     * ```js
+     * bot.on('error', err => console.log(err.message));
+     * ```
+     */
+    emit<K extends keyof EventEmitter>(eventName: K, data?: EventEmitter[K]): void {
+        if (!this.handlers[eventName]) this.handlers[eventName] = [];
+        this.handlers[eventName].forEach((handler: any) => handler(data));
+    }
+}
