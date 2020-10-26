41a06dafedb393d05d26afa81c5d6e181e2b6d3b
JanotLeLapin <joseph300905@gmail.com>
2020-10-26 19:24:27 +0100

    Compile TypeScript

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,11 +3,4 @@ import User from './Structures/User';
 import Guild from './Structures/Guild';
 import Channel from './Structures/Channel';
 import Message from './Structures/Message';
-declare const _default: {
-    Bot: typeof Bot;
-    User: typeof User;
-    Guild: typeof Guild;
-    Channel: typeof Channel;
-    Message: typeof Message;
-};
-export default _default;
+export { Bot, User, Guild, Channel, Message, };
--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Message = exports.Channel = exports.Guild = exports.User = exports.Bot = void 0;
 var Bot_1 = __importDefault(require("./Bot/Bot"));
+exports.Bot = Bot_1.default;
 var User_1 = __importDefault(require("./Structures/User"));
+exports.User = User_1.default;
 var Guild_1 = __importDefault(require("./Structures/Guild"));
+exports.Guild = Guild_1.default;
 var Channel_1 = __importDefault(require("./Structures/Channel"));
+exports.Channel = Channel_1.default;
 var Message_1 = __importDefault(require("./Structures/Message"));
-exports.default = {
-    Bot: Bot_1.default,
-    User: User_1.default,
-    Guild: Guild_1.default,
-    Channel: Channel_1.default,
-    Message: Message_1.default,
-};
+exports.Message = Message_1.default;
