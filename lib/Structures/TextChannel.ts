c5a6e849a9b19aa1da83c096b5f02035b53b6471
JanotLeLapin <joseph300905@gmail.com>
2020-10-26 18:43:58 +0100

    Create TextChannel class

--- lib/Structures/TextChannel.ts
+++ lib/Structures/TextChannel.ts
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
+const baseUrl = 'https://discord.com/api/channels/';
+
+import Bot from '../Bot/Bot';
+
+import Channel from './Channel';
+import Message from './Message';
+
 export class TextChannelBase extends Channel {
     lastMessageID: string;
 
@@ -23,5 +32,29 @@ export class TextChannelBase extends Channel {
                 .then(reply => resolve(new Message(reply.data, this.b, this.token)))
                 .catch(err => reject(err.response));
         })
+    }
+}
+
+export default class TextChannel extends TextChannelBase {
+    guildID:              string;
+    name:                 string;
+    position:             number;
+    permissionOverwrites: any[];
+    rateLimitPerUser:     number;
+    nsfw:                 boolean;
+    topic:                string;
+    parentID:             string;
+
+    constructor (data: any, bot: Bot, token: string) {
+        super(data, bot, token);
+
+        this.guildID = data.guild_id;
+        this.name = data.name;
+        this.position = data.position;
+        this.permissionOverwrites = data.permission_overwrites;
+        this.rateLimitPerUser = data.rate_limit_per_user;
+        this.nsfw = data.nsfw
+        this.topic = data.topic;
+        this.parentID = data.parent_id;
     }
 }
