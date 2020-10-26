b31fb40467eada48138cf6d5221cd486b1408a34
JanotLeLapin <joseph300905@gmail.com>
2020-10-26 18:41:50 +0100

    Create User class

---
+++ lib/Structures/User.ts
@@ -0,0 +1,77 @@
+import axios from 'axios';
+
+import Bot from '../Bot/Bot';
+
+import DMChannel from './DMChannel';
+import Message from './Message';
+
+const baseUrl = 'https://discord.com/api/users/';
+
+type premiumType = 0 | 1 | 2;
+
+export default class User {
+    protected b:     Bot;
+    protected token: string;
+
+    id:            string;
+    username:      string;
+    discriminator: string;
+    avatar:        string;
+    bot?:          boolean;
+    system?:       boolean;
+    mfaEnabled?:   boolean;
+    locale?:       string;
+    verified?:     boolean;
+    email?:        string;
+    flags?:        number;
+    premiumType?:  premiumType;
+    publicFlags?:  number;
+
+    constructor (data: any, bot: Bot, token: string) {
+        this.b = bot;
+        this.token = token;
+
+        this.id = data.id;
+        this.username = data.username;
+        this.discriminator = data.discriminator;
+        this.avatar = data.avatar;
+        this.bot = data.bot || false;
+        this.system = data.system || false;
+        this.mfaEnabled = data.mfa_enabled || false;
+        this.locale = data.locale;
+        this.verified = data.verified || false;
+        this.email = data.email;
+        this.flags = data.flags;
+        this.premiumType = data.premiumType;
+        this.publicFlags = data.publicFlags;
+    }
+
+    /**
+     * @description The avatar url obtained from the user ID and avatar hash
+     */
+    avatarURL () {
+        return 'https://cdn.discordapp.com/avatars/' + this.id + '/' + this.avatar;
+    }
+
+    /**
+     * @description Send a message to the user
+     * @param {string} message The message to send
+     */
+    send (message: string): Promise<Message> {
+        return new Promise((resolve, reject) => {
+            axios.post(baseUrl + '@me/channels', {
+                recipient_id: this.id,
+            }, {
+                headers: {
+                    Authorization: 'Bot ' + this.token,
+                },
+            })
+                .then(dm => {
+                    new DMChannel(dm.data, this.b, this.token).send(message)
+                        .then(reply => resolve(reply))
+                        .catch(err => reject(err.response));
+                })
+                .catch(err => reject(err));
+        })
+    }
+}
