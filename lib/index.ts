970e1ccac1096eaaaa61247729e5d6bf1c3f3b80
Joseph <joseph300905@gmail.com>
2020-11-01 11:23:05 +0100

    Add classes to index export

--- lib/index.ts
+++ lib/index.ts
@@ -2,14 +2,19 @@ import Bot from './Bot/Bot';
 
 import User from './Structures/User';
 import Guild from './Structures/Guild';
+import Member from './Structures/Member';
+import Role from './Structures/Role';
 import Channel from './Structures/Channel';
 import Message from './Structures/Message';
 import Embed from './Structures/Embed';
 
 export {
     Bot,
+
     User,
     Guild,
+    Member,
+    Role,
     Channel,
     Message,
     Embed,
